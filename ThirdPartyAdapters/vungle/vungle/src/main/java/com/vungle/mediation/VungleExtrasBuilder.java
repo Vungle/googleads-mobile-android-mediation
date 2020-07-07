36e909e3702647b714103c8c03d1785726b0cb34
Alexander Gubov <alexander.gubov@vungle.com>
2020-07-07 22:48:56 +0300

    [AND-2932] revert default value

--- ThirdPartyAdapters/vungle/vungle/src/main/java/com/vungle/mediation/VungleExtrasBuilder.java
+++ ThirdPartyAdapters/vungle/vungle/src/main/java/com/vungle/mediation/VungleExtrasBuilder.java
@@ -87,7 +87,7 @@ public final class VungleExtrasBuilder {
       adConfig.setMuted(networkExtras.getBoolean(EXTRA_START_MUTED, false));
       adConfig.setFlexViewCloseTime(networkExtras.getInt(EXTRA_FLEXVIEW_CLOSE_TIME, 0));
       adConfig.setOrdinal(networkExtras.getInt(EXTRA_ORDINAL_VIEW_COUNT, 0));
-      adConfig.setAdOrientation(networkExtras.getInt(EXTRA_ORIENTATION, AdConfig.MATCH_VIDEO));
+      adConfig.setAdOrientation(networkExtras.getInt(EXTRA_ORIENTATION, AdConfig.AUTO_ROTATE));
     }
     return adConfig;
   }
