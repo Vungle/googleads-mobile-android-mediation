e050259ad775954d73deed4ab012b6fb008bb871
Stanley Seeto <sseeto@liftoff.io>
2026-01-21 14:12:19 -0800

    Testing native lifecycle

---
+++ ThirdPartyAdapters/liftoffmonetize/liftoffmonetize/build/generated/source/buildConfig/androidTest/debug/com/vungle/mediation/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.vungle.mediation.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.vungle.mediation.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String ADAPTER_VERSION = "7.6.2.0";
+}
