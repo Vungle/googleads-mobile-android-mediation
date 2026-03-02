f6a464f1f40da49511d32b8264ecb7b3afb96a0e
Akifumi Shinagawa <ashinagawa@liftoff.io>
2026-03-02 15:16:11 -0800

    Fix the ut issue

--- ThirdPartyAdapters/liftoffmonetize/liftoffmonetize/src/main/java/com/google/ads/mediation/vungle/waterfall/VungleWaterfallBannerAd.kt
+++ ThirdPartyAdapters/liftoffmonetize/liftoffmonetize/src/main/java/com/google/ads/mediation/vungle/waterfall/VungleWaterfallBannerAd.kt
@@ -32,6 +32,6 @@ class VungleWaterfallBannerAd(
     bannerAdView: VungleBannerView,
     mediationBannerAdConfiguration: MediationBannerAdConfiguration,
   ) {
-    bannerAdView.load(null)
+    bannerAdView.load(null as String?)
   }
 }
